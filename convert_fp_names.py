94ba80ed5b21ce7555cfcfd95437c5f2697d00d3
Josh Mandzak <jmandzak99@gmail.com>
2025-07-31 20:57:52 -0400

    add data from 2023

--- convert_fp_names.py
+++ convert_fp_names.py
@@ -1,8 +1,10 @@
 import pandas as pd
 from FootballNameMatcher import match_name
 
-PPR = False
+PPR = True
 PPR_STRING = "_PPR" if PPR else "_standard"
+YEAR = 23
+YEAR_STRING = f"_{YEAR:02d}"
 
 
 def get_df(file_path: str) -> pd.DataFrame:
@@ -21,10 +23,12 @@ def remove_rows_with_no_name(df: pd.DataFrame) -> pd.DataFrame:
 
 
 def main():
-    df = get_df(f"data/FantasyPros_Fantasy_Football_Points{PPR_STRING}.csv")
+    df = get_df(
+        f"data/FantasyPros_Fantasy_Football_Points{PPR_STRING}{YEAR_STRING}.csv"
+    )
     df = convert_fp_names(df)
     df = remove_rows_with_no_name(df)
-    df.to_csv(f"data/fp_converted_names{PPR_STRING}.csv", index=False)
+    df.to_csv(f"data/fp_converted_names{PPR_STRING}{YEAR_STRING}.csv", index=False)
 
 
 if __name__ == "__main__":
